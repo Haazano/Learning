fcb7ac248820e6e5effd7dc6214360949fe464d5
Aleksandr Streltsov <jness.pro@gmail.com>
2019-12-04 02:39:38 +0300

    vars & for loops

--- lesson_1/src/main/java/com/haazano/lesson1/App.java
+++ lesson_1/src/main/java/com/haazano/lesson1/App.java
@@ -1,5 +1,18 @@
 package com.haazano.lesson1;
 
-public class App {
+import java.util.ArrayList;
 
+public class App {
+    public static void main(String[] args) {
+        TheBigBang theBigBang10 = new TheBigBang("Певый ББ", 10);
+        TheBigBang theBigBang20 = new TheBigBang("Второй ББ", 20);
+
+        ArrayList<TheBigBang> bangs = new ArrayList<>();
+        bangs.add(theBigBang10);
+        bangs.add(theBigBang20);
+
+        for (TheBigBang bang : bangs) {
+            System.out.println(bang.getName() + " с массой " + bang.getMass());
+        }
+    }
 }
---
+++ lesson_1/src/main/java/com/haazano/lesson1/TheBigBang.java
@@ -0,0 +1,27 @@
+package com.haazano.lesson1;
+
+public class TheBigBang {
+    private String name;
+    private Integer mass;
+
+    public TheBigBang(String name, Integer mass) {
+        this.name = name;
+        this.mass = mass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getMass() {
+        return mass;
+    }
+
+    public void setMass(Integer mass) {
+        this.mass = mass;
+    }
+}
